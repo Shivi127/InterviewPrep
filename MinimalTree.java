67cdc9ae67d990b7449df23d514bbabd6d90b0c8
Shivangi Singh <singhnight@gmail.com>
2018-01-13 02:52:48 -0500

    Add files via upload
    *Minimal Tree- Given a sorted array (Increasing Order) with unique integer elements, write 
     *an algorithm to create a Binary Search Tree with minimal height

---
+++ MinimalTree.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author shivangisingh
+ *
+ */
+public class MinimalTree {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
